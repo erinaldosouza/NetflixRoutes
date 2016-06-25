3dd9a0e52872a65dad0081ff49e7163d55d5595c
elfiservice <elfiservice@hotmail.com>
2016-06-25 11:03:21 -0300

    alterado pagina Inicial e CSS do Slide responsivo

--- controller/home-index-controller.js
+++ controller/home-index-controller.js
@@ -2,6 +2,9 @@ angular.module('NetflixRouter')
        .controller('HomeIndexController', function($http) {
             this.preload = false;
             $http.get("#/index");
+			
+
+			
         });
 
 
@@ -12,3 +15,15 @@ angular.module('NetflixRouter')
         this.showForm() {
             
         }*/
+		
+//var app = angular.module("NetflixRouter", ["ngResource"]);
+
+function MyCtrl($scope, $resource) {
+			  var TwitterAPI = $resource("http://search.twitter.com/search.json",
+    { callback: "JSON_CALLBACK" },
+    { get: { method: "JSONP" }});
+
+  $scope.search = function() {
+    $scope.searchResult = TwitterAPI.get({ q: $scope.searchTerm });
+  };
+}
